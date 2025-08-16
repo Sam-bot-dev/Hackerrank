e6db33822e1b4d0ad96cdb92df2109c9ae8be5d6
Sammy <bavesh2006kumar@gmail.com>
2025-08-16 14:33:39 +0000

    .

---
+++ mutation.py
@@ -0,0 +1,15 @@
+def mutate_string(string, position, character):
+    string= s
+    pos= int(i) 
+    l = list(string)
+    l[pos] = c
+    string = "".join(l)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+''' input would be first string like  abracadabra then the position and character like - 5,k'''
