7059dded262da475d398e5831167d92cd19fc82a
Sammy <bavesh2006kumar@gmail.com>
2025-08-16 18:22:45 +0000

    .

---
+++ difference.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+setn = set(input().split())
+b = int(input())
+setb = set(input().split())
+print(len(setn.difference(setb)))
---
+++ symmetric_difference.py
@@ -0,0 +1,5 @@
+'''.symmetric_difference()
+The .symmetric_difference() operator returns a set with all the elements that are in the set and the iterable but not both.
+Sometimes, a ^ operator is used in place of the .symmetric_difference() tool, but it only operates on the set of elements in set.
+The set is immutable to the .symmetric_difference() operation (or ^ operation).
+'''
