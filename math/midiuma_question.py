3e11a9ce1be012c85b1e87b2bc4c578aaf9dc13f
Sammy <bavesh2006kumar@gmail.com>
2025-08-16 19:23:45 +0000

    .

---
+++ math/midiuma_question.py
@@ -0,0 +1,11 @@
+import math
+
+ab = float(input())
+bc = float(input())
+
+cos_val = bc / math.sqrt(ab**2 + bc**2)
+angle_rad = math.acos(cos_val)
+angle_deg = math.degrees(angle_rad)
+print(str(round(angle_deg))+"\u00B0")
+
+
